ea6c17e9b00b5f2831490f2713063db96296d064
Justas Versekys <jusvrs@telesoftas.com>
2018-08-21 09:55:52 +0300

    guilded rose some additional initial data.

--- src/main/java/com/gildedrose/component/InitializingElasticBean.java
+++ src/main/java/com/gildedrose/component/InitializingElasticBean.java
@@ -18,6 +18,14 @@ public class InitializingElasticBean implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        itemsService.save(Item.builder().name("Aged Brie").sellIn(15).quality(3).build());
+        itemsService.save(Item.builder().name("+5 Dexterity Vest").sellIn(10).quality(20).build());
+        itemsService.save(Item.builder().name("Aged Brie").sellIn(2).quality(0).build());
+        itemsService.save(Item.builder().name("Elixir of the Mongoose").sellIn(5).quality(7).build());
+        itemsService.save(Item.builder().name("Sulfuras, Hand of Ragnaros").sellIn(0).quality(80).build());
+        itemsService.save(Item.builder().name("Sulfuras, Hand of Ragnaros").sellIn(-1).quality(80).build());
+        itemsService.save(Item.builder().name("Backstage passes to a TAFKAL80ETC concert").sellIn(15).quality(20).build());
+        itemsService.save(Item.builder().name("Backstage passes to a TAFKAL80ETC concert").sellIn(10).quality(49).build());
+        itemsService.save(Item.builder().name("Backstage passes to a TAFKAL80ETC concert").sellIn(5).quality(49).build());
+        itemsService.save(Item.builder().name("Conjured Mana Cake").sellIn(3).quality(6).build());
     }
 }
